9831d1503b72fa60d22eae77fb7918f2ad521dd6
shreeyabekkam <shreeyabekkam@gmail.com>
2022-11-17 10:37:18 +0530

    first commit to maven java project

---
+++ src/main/java/com/javaproject/JavaProject/App.java
@@ -0,0 +1,13 @@
+package com.javaproject.JavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from 20BD1A66B2" );
+    }
+}
